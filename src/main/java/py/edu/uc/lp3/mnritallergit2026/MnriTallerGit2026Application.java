0fb4b63f001a06f75f107ec2c48d55d3b664e899
mnri1904 <marcenicoromero@gmail.com>
2026-09-16 13:50:16 -0300

    chore: esqueleto Spring Boot API REST desde start.spring.io

---
+++ src/main/java/py/edu/uc/lp3/mnritallergit2026/MnriTallerGit2026Application.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.mnritallergit2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnriTallerGit2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnriTallerGit2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/py/edu/uc/lp3/mnritallergit2026/MnriTallerGit2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.mnritallergit2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnriTallerGit2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
